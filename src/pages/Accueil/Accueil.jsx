0f6409f65b2fc72fd84aae66ce41538572df7b3c
NicolaBaker <nicolabak05@gmail.com>
2025-05-30 14:43:06 -0400

    Revenir a img ajout

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -59,34 +59,34 @@ export default function Accueil() {
       <div className={styles.sectionApropo} id="section3">
         <h1>- L’équipe du Salon Boréal -</h1>
         <br />
-        <div className={styles.equipe-container}>
+        <div className="equipe-container">
           <div className="equipe">
-            <img src="./img/equipe/anne.jpg" alt="Anne" />
+            <img src="/img/equipe/anne.jpg" alt="Anne" />
             <h3>Anne</h3>
             <p>Directrice du salon</p>
           </div>
           <div className="equipe">
-            <img src="./img/equipe/Audrey.jpg" alt="Audrey" />
+            <img src="/img/equipe/Audrey.jpg" alt="Audrey" />
             <h3>Audrey</h3>
             <p>Experte coupe & mèches</p>
           </div>
           <div className="equipe">
-            <img src="./img/equipe/Karim.jpg" alt="Karim" />
+            <img src="/img/equipe/Karim.jpg" alt="Karim" />
             <h3>Karim</h3>
             <p>Experte coupe & mèches</p>
           </div>
           <div className="equipe">
-            <img src="./img/equipe/cami.jpg" alt="Catherine" />
+            <img src="/img/equipe/cami.jpg" alt="Catherine" />
             <h3>Catherine</h3>
             <p>Experte coupe & mèches</p>
           </div>
           <div className="equipe">
-            <img src="./img/equipe/julie.jpg" alt="Julie" />
+            <img src="/img/equipe/julie.jpg" alt="Julie" />
             <h3>Julie</h3>
             <p>Experte coupe & mèches</p>
           </div>
           <div className="equipe">
-            <img src="./img/equipe/lucie.jpg" alt="Lucie" />
+            <img src="/img/equipe/lucie.jpg" alt="Lucie" />
             <h3>Lucie</h3>
             <p>Experte coupe & mèches</p>
           </div>
