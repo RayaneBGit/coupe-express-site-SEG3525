75caaf158f8caf088583147368e533882f842e03
NicolaBaker <nicolabak05@gmail.com>
2025-06-03 19:59:32 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,56 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer} id="footer">
+    <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <div className={styles.footerText}>
-          <h3>Gardons le contact.</h3>
-          <p>...</p>
+          <h2>Salon Boréal</h2>
+          <p>Un moment pour vous, entre de bonnes mains.</p>
+        </div>
+
+        <div className={styles.footerLogo}>
+          <img src="/img/logopn.png" alt="Boréal" />
+        </div>
+
+        <div className={styles.footerInfos}>
+          <ul>
+            <li>
+              <span className={styles.icon}>
+                <img src="/img/marker.png" alt="Adresse" />
+              </span>
+              <div className={styles.info}>
+                <strong>Adresse</strong>
+                <br />
+                123 Rue Principale,<br/>
+                Moose Creek, ON K0C 1W0
+              </div>
+            </li>
+            <li>
+              <span className={styles.icon}>
+                <img src="/img/phone-call.png" alt="Téléphone" />
+              </span>
+              <div className={styles.info}>
+                <strong>Téléphone</strong>
+                <br />
+                <a href="tel:6135554567">(613) 555-4567</a>
+              </div>
+            </li>
+            <li>
+              <span className={styles.icon}>
+                <img src="/img/envelope (1).png" alt="Courriel" />
+              </span>
+              <div className={styles.info}>
+                <strong>Courriel</strong>
+                <br />
+                <a href="mailto:info@boreal.ca">info@boreal.ca</a>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
+
       <div className={styles.footerBottom}>
-        &copy; 2025 Boréal | Tous droits réservés
+        &copy; 2025 SalonBoréal | Tous droits réservés
       </div>
     </footer>
   );
