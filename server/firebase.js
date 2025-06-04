fe09558f3b877bceb94984e4bfa071b9cb1a9e3a
Nicola Baker <145513846+NicolaBaker@users.noreply.github.com>
2025-06-04 20:17:04 +0000

    Enleve firebase

--- server/firebase.js
+++ server/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,3 @@ const firebaseConfig = {
   measurementId: "G-GBK702F8DN"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
